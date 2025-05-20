231bdf21c8ced6948105a17ee808378a66804df0
Phan Tuấn Thạch - B20DCCN635 <98653656+phanthach@users.noreply.github.com>
2025-05-21 01:28:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function saveNameAndRedirect() {
   const name = document.getElementById('nameInput').value.trim();
   if (name) {
-    fetch("https://getform.io/f/bronljda", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body: new URLSearchParams({ Name: name })  // lưu ý chữ "Name" viết hoa như bạn dùng trong URL query
-    })
-    .then(res => {
-      if (res.ok) {
-        console.log("Gửi tên thành công!");
+    const url = `https://getform.io/f/bronljda?Name=${encodeURIComponent(name)}`;
+
+  // Gửi request GET
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        console.log('Gửi dữ liệu thành công');
       } else {
-        console.error("Lỗi khi gửi tên");
+        console.log('Lỗi khi gửi dữ liệu');
       }
     })
-    .catch(err => console.error("Lỗi kết nối:", err));
+    .catch(error => console.error('Lỗi mạng:', error));
+} else {
+  console.log('Không có tên trong localStorage');
+  }
 
     localStorage.setItem('guestName', name);
     window.location.href = "invite.html";
